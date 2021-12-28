4b048a6dc3898ef8e4d4ce6196dc00dc5faacd08
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2021-12-28 11:14:06 -0800

    inheritance - extends + method override

---
+++ inheritance_08/index.php
@@ -0,0 +1,32 @@
+<?php
+   class UserData{
+       public $user;
+       public $userId;
+
+       public function __construct($userName, $userId){
+           $this->user = $userName;
+           $this->userId = $userId;
+       }
+
+       public function display(){
+           echo "Username is {$this->user} and userid is {$this->userId}";
+       }
+   }
+
+   class Admin extends UserData{
+       public $level;
+
+       public function display(){
+        echo "Username is {$this->user} and userid is {$this->userId} and level {$this->level}";
+    }
+   }
+
+   $userDataObj = new Userdata('ParentClass',15);
+   $userDataObj->display();
+
+   echo '<br/>';
+
+   $adminObj = new Admin('subClass',10);
+   $adminObj->level = "admin";
+   $adminObj->display();
+?>
