f79ecc154b2912d9644dae1f75e3744929f6a616
EhfazAdnan <ehfaz15-167@diu.edu.bd>
2022-01-01 10:01:04 -0800

    interface with example

--- interface_12/index.php
+++ interface_12/index.php
@@ -1,3 +1,58 @@
 <?php
 
 // interface - keyword implements
+
+interface School{
+    public function mySchool();
+}
+
+interface College{
+    public function myCollege();
+}
+
+interface Varcity{
+    public function myVarcity();
+}
+
+class Teacher implements School, College, Varcity{
+    public function __construct(){
+       $this->mySchool();
+       $this->myCollege();
+       $this->myVarcity();
+    }
+
+    public function mySchool(){
+        echo "I am a school teacher<br>";
+    }
+
+    public function myCollege(){
+        echo "I am a college teacher<br>";
+    }
+
+    public function myVarcity(){
+        echo "I am a vercity teacher<br>";
+    }
+}
+
+class Teacher2 implements School, College, Varcity{
+    public function __construct(){
+       $this->mySchool();
+       $this->myCollege();
+       $this->myVarcity();
+    }
+
+    public function mySchool(){
+        echo "I am a school teacher<br>";
+    }
+
+    public function myCollege(){
+        echo "I am a college teacher<br>";
+    }
+
+    public function myVarcity(){
+        echo "I am a vercity teacher<br>";
+    }
+}
+
+$teacher = new Teacher();
+$teacher = new Teacher2();
